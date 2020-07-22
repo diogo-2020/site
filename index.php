e9fb649d4aa96c82d006cfea4249c901e3a57245
diogo-2020 <diogotigrejw@gmail.com>
2020-07-22 15:53:52 -0300

    Versão 0.1.5 Corresão feita erro estava em index.php

--- index.php
+++ index.php
@@ -1,31 +1,25 @@
 <?php 
-
+ 
 require_once("vendor/autoload.php");
-
+ 
 use \Slim\Slim;
 use \Hcode\Page;
-use \hcode\PageAdmin;
-
+use \Hcode\PageAdmin;
+ 
 $app = new Slim();
-
 $app->config('debug', true);
-
+ 
 $app->get('/', function() {
-    
-		$page = new Page();
-
-		$page->setTpl("index");
-
+    $page = new Page();
+    $page->setTpl("index");
 });
-
-/*$app->get('/', function() {
-    
-		$page = new PageAdmin();
-
-		$page->setTpl("index");
-
+ 
+$app->get('/admin', function() {
+    $page = new PageAdmin();
+    $page->setTpl("index");
+});
+ 
 $app->run();
-
-});*/
-
- ?>
+ 
+?>
+ 
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -7,10 +7,6 @@ namespace Composer\Autoload;
 class ComposerStaticInit6246f2aa5f43fda1b78e6b2db6880335
 {
     public static $prefixLengthsPsr4 = array (
-        'P' => 
-        array (
-            'PHPMailer\\PHPMailer\\' => 20,
-        ),
         'H' => 
         array (
             'Hcode\\' => 6,
@@ -18,10 +14,6 @@ class ComposerStaticInit6246f2aa5f43fda1b78e6b2db6880335
     );
 
     public static $prefixDirsPsr4 = array (
-        'PHPMailer\\PHPMailer\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/phpmailer/phpmailer/src',
-        ),
         'Hcode\\' => 
         array (
             0 => __DIR__ . '/..' . '/hcodebr/php-classes/src',
@@ -45,12 +37,24 @@ class ComposerStaticInit6246f2aa5f43fda1b78e6b2db6880335
         ),
     );
 
+    public static $classMap = array (
+        'EasyPeasyICS' => __DIR__ . '/..' . '/phpmailer/phpmailer/extras/EasyPeasyICS.php',
+        'PHPMailer' => __DIR__ . '/..' . '/phpmailer/phpmailer/class.phpmailer.php',
+        'PHPMailerOAuth' => __DIR__ . '/..' . '/phpmailer/phpmailer/class.phpmaileroauth.php',
+        'PHPMailerOAuthGoogle' => __DIR__ . '/..' . '/phpmailer/phpmailer/class.phpmaileroauthgoogle.php',
+        'POP3' => __DIR__ . '/..' . '/phpmailer/phpmailer/class.pop3.php',
+        'SMTP' => __DIR__ . '/..' . '/phpmailer/phpmailer/class.smtp.php',
+        'ntlm_sasl_client_class' => __DIR__ . '/..' . '/phpmailer/phpmailer/extras/ntlm_sasl_client.php',
+        'phpmailerException' => __DIR__ . '/..' . '/phpmailer/phpmailer/class.phpmailer.php',
+    );
+
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
             $loader->prefixLengthsPsr4 = ComposerStaticInit6246f2aa5f43fda1b78e6b2db6880335::$prefixLengthsPsr4;
             $loader->prefixDirsPsr4 = ComposerStaticInit6246f2aa5f43fda1b78e6b2db6880335::$prefixDirsPsr4;
             $loader->prefixesPsr0 = ComposerStaticInit6246f2aa5f43fda1b78e6b2db6880335::$prefixesPsr0;
+            $loader->classMap = ComposerStaticInit6246f2aa5f43fda1b78e6b2db6880335::$classMap;
 
         }, null, ClassLoader::class);
     }
